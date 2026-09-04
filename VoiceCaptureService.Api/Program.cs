2ada3d2f0cccfaba5f7d64d8f5edfc68c3449951
Karthikeya Sundaramoorthy <sukarthilenovo@outlook.com>
2026-09-04 01:07:47 -0400

    Added Aspire ServiceDefaults integration.

--- VoiceCaptureService.Api/Program.cs
+++ VoiceCaptureService.Api/Program.cs
@@ -11,6 +11,8 @@ using VoiceCaptureService.Infrastructure.Recording.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddServiceDefaults();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
